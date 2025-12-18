e5a1175abff39756f4f79d8cb8d881ad827a2685
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-12-17 21:27:40 -0300

    feat: update local_settings.example.py to refine allowed hosts and CSRF trusted origins

--- project/local_settings.example.py
+++ project/local_settings.example.py
@@ -3,15 +3,12 @@ SECRET_KEY = 'CHANGE-ME'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '127.0.0.1', 
-    'localhost'
+    '127.0.0.1',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://127.0.0.1:8000', 
-    'http://127.0.0.1:8000', 
-    'https://localhost:8000',
-    'http://localhost:8000'
+    'https://127.0.0.1:8000',
+    'http://127.0.0.1:8000',
 ]
 
 ENCRYPTION_KEY = 'CHANGE-ME'
