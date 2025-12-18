190b75eb0e086250d240320450c03c3dc3f6b571
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-12-18 08:32:25 -0300

    feat: comment out LoginsForm in LoginsAdmin for future reference

--- app/admin.py
+++ app/admin.py
@@ -7,7 +7,7 @@ admin.site.register(LoginTypes)
 
 @admin.register(Logins)
 class LoginsAdmin(admin.ModelAdmin):
-    form = LoginsForm
+    # form = LoginsForm
     list_display = ('service', 'type', 'login', 'updated_at', 'created_at')
     search_fields = ('service', 'type__title', 'login')
     list_filter = ('type',)
