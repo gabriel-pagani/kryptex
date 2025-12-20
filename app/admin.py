f027568426b2f89f46a4a3771b90f2df116eb437
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-12-20 00:40:45 -0300

    feat: remove LoginsForm and associated decryption logic from forms.py; clean up admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from .models import Logins, LoginTypes
-from .forms import LoginsForm
 
 
 admin.site.register(LoginTypes)
 
 @admin.register(Logins)
 class LoginsAdmin(admin.ModelAdmin):
-    # form = LoginsForm
     list_display = ('service', 'type', 'login', 'updated_at', 'created_at')
     search_fields = ('service', 'type__title', 'login')
     list_filter = ('type',)
