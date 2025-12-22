7fefe61ae3c2ff7d8d616fe395d8da5bc4046dcf
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-12-22 15:50:27 -0300

    style: improve code formatting

--- app/admin.py
+++ app/admin.py
@@ -4,6 +4,7 @@ from .models import Logins, LoginTypes
 
 admin.site.register(LoginTypes)
 
+
 @admin.register(Logins)
 class LoginsAdmin(admin.ModelAdmin):
     list_display = ('service', 'type', 'login', 'updated_at', 'created_at')
--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include('app.urls', namespace='app')),
-    path('admin/', admin.site.urls), 
+    path('admin/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
