780cbe89ee878e3004272c715afd9d799651586c
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-12-24 19:49:45 -0300

    Remove unused Logins model and associated files; clean up admin and tests

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from .models import Logins, LoginTypes
+from .models import LoginTypes
 
 
 admin.site.register(LoginTypes)
-
-
-@admin.register(Logins)
-class LoginsAdmin(admin.ModelAdmin):
-    list_display = ('service', 'type', 'login', 'updated_at', 'created_at')
-    search_fields = ('service', 'type__title', 'login')
-    list_filter = ('type',)
