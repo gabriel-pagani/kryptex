48a3209f4ba7ae71efa619650e8a75093297bed6
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-08 14:03:05 -0300

    Add AES encryption and decryption functions for enhanced password security

--- src/utils/crypto.py
+++ src/utils/crypto.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
-from argon2 import PasswordHasher
+from argon2 import PasswordHasher, low_level
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 load_dotenv()
 
@@ -27,3 +28,31 @@ def verify_hash(password_hash: str, password: str) -> bool:
         return password_hasher.verify(password_hash, password + PEPPER)
     except Exception as e:
         return False
+
+
+def derive_master_password(master_password: str, salt: bytes) -> bytes:
+    return low_level.hash_secret_raw(
+        secret=master_password.encode(),
+        salt=salt,
+        time_cost=12,
+        memory_cost=262144,
+        parallelism=12,
+        hash_len=32,
+        type=low_level.Type.ID
+    )
+
+
+def encrypt_password(master_password: str, salt: bytes, password: str) -> tuple[bytes, bytes]:
+    aesgcm = AESGCM(derive_master_password(master_password, salt))
+    iv = os.urandom(12)
+    password_encrypted = aesgcm.encrypt(iv, password.encode(), None)
+    return (iv, password_encrypted)
+
+
+def decrypt_password(master_password: str, salt: bytes, iv: bytes, password_encrypted: bytes) -> str:
+    try:
+        aesgcm = AESGCM(derive_master_password(master_password, salt))
+        password_decrypted = aesgcm.decrypt(iv, password_encrypted, None).decode()
+        return password_decrypted
+    except Exception as e:
+        return ""
