ff7e05c8732bf5a9a82afbec28a7d998b2e7864f
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-14 20:37:58 -0300

    Add show_message function to display messages with different color codes

---
+++ src/utils/ui.py
@@ -0,0 +1,20 @@
+import flet as ft
+
+
+def show_message(self, type: int, message: str):
+    colors = {
+        1: [ft.Colors.GREEN, ft.Colors.WHITE],  # Success
+        2: [ft.Colors.YELLOW, ft.Colors.BLACK], # Warning
+        3: [ft.Colors.RED, ft.Colors.WHITE],    # Error
+        4: [ft.Colors.GREY, ft.Colors.BLACK],   # Info
+    }
+
+    self.page.overlay.append(
+        ft.SnackBar(
+            content=ft.Text(message, color=colors[type][1]),
+            bgcolor=colors[type][0],
+            open=True
+        )
+    )
+
+    self.page.update()
