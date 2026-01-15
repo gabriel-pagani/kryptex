87a4408d7bc2c3611de5cf0503a262f7310b98dc
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-14 22:21:41 -0300

    Refactor main entry point to use Main class for improved structure and clarity

--- src/main.py
+++ src/main.py
@@ -1,9 +1,36 @@
 import flet as ft
-from views.app import App
+from views.auth import AuthView
+from views.home import HomeView
+
+
+class Main:
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.setup_page()
+        self.auth_view()
+
+    def setup_page(self):
+        self.page.title = 'Kryptex'
+        self.page.window.icon = r'favicon.png'
+        self.page.theme_mode = ft.ThemeMode.LIGHT
+        self.page.bgcolor = ft.Colors.WHITE
+        self.page.padding = 0
+        self.page.window.width = 600
+        self.page.window.height = 750
+        self.page.update()
+
+    def auth_view(self):
+        AuthView(self.page, on_login_success=self.home_view).show_login()
+
+    def home_view(self, user, user_key):
+        HomeView(self.page, user, user_key, on_logout=self.logout).show_home()
+
+    def logout(self, e):
+        self.auth_view()
 
 
 def main(page: ft.Page):
-    App(page)
+    Main(page)
 
 
 ft.run(main, assets_dir="assets")
